7e406a4752611938e80a9f988e5328749825e9e0
coma-dwc <tamagomapon@gmail.com>
2023-06-26 16:58:59 +0900

    section4-27 finish

--- 04_react_basic/src/073_practice_component/start/Example.js
+++ 04_react_basic/src/073_practice_component/start/Example.js
@@ -1,15 +1,26 @@
+import Child from './components/Child';
+
 const Example = () => {
-  return (
-    <div>
-      <h3>練習問題</h3>           
-      <p>/componentsフォルダをstart内に作成して、Childにコンポーネントを分離して、Exampleコンポーネントで読み込んでください。</p>
-      { /* ↓↓↓ Childコンポーネントに移動 */ }
-      <div className="component">
-        <h3>Child Component</h3>
-      </div>
-      { /* ↑↑↑ Childコンポーネントに移動 */ }
-    </div>
-  );
+  return  <Child />
 };
 
 export default Example;
+
+
+// 練習問題（コード）
+
+// const Example = () => {
+//   return (
+//     <div>
+//       <h3>練習問題</h3>           
+//       <p>/componentsフォルダをstart内に作成して、Childにコンポーネントを分離して、Exampleコンポーネントで読み込んでください。</p>
+//       { /* ↓↓↓ Childコンポーネントに移動 */ }
+//       <div className="component">
+//         <h3>Child Component</h3>
+//       </div>
+//       { /* ↑↑↑ Childコンポーネントに移動 */ }
+//     </div>
+//   );
+// };
+ 
+// export default Example;
