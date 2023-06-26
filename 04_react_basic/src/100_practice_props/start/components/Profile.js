1a37cd3f1104acd60f415a39cb3a1de7d0073f85
coma-dwc <tamagomapon@gmail.com>
2023-06-26 23:04:06 +0900

    section4-34 finish

--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,12 +1,28 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({ name = "John Doe", age = "??", country = "Japan" }) => {
   return (
     <div className="profile">
-
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <h3>From: {country}</h3>
     </div>
   );
 };
 
 
 export default Profile;
+
+// 元々の記述
+// import "./Profile.css";
+
+// const Profile = (props) => {
+//   return (
+//     <div className="profile">
+
+//     </div>
+//   );
+// };
+
+
+// export default Profile;
