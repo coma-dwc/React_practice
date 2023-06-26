e2f268d0ecfa32585d1d35be0c010141c282dd44
coma-dwc <tamagomapon@gmail.com>
2023-06-26 22:31:38 +0900

    section4-32 finish

--- 04_react_basic/src/090_props/start/Example.js
+++ 04_react_basic/src/090_props/start/Example.js
@@ -1,5 +1,19 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => {
+    return (
+        <>
+            <Child color="" />
+            <Child color="red" />
+        </>
+    )
+};
 
 export default Example;
+
+// 元々の記述
+// import Child from "./components/Child";
+
+// const Example = () => <Child />;
+
+// export default Example;
--- 04_react_basic/src/090_props/start/components/Child.js
+++ 04_react_basic/src/090_props/start/components/Child.js
@@ -5,12 +5,33 @@
 
 import "./Child.css";
 
-const Child = () => {
+const Child = (props) => {
+
+  console.log(props);
+
   return (
-    <div className="component">
+    <div className={`component ${props.color}`}>
       <h3>Hello Component</h3>
     </div>
   );
 };
 
 export default Child;
+
+// 元々の記述
+// /* POINT 式と文
+// 式：何らかの値を返すもの（変数に代入できるもの）
+// 文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
+// */
+
+// import "./Child.css";
+
+// const Child = () => {
+//   return (
+//     <div className="component">
+//       <h3>Hello Component</h3>
+//     </div>
+//   );
+// };
+
+// export default Child;
