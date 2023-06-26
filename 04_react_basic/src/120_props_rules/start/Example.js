83c9c23cb418a87531cb2ea30dbac20206aca238
coma-dwc <tamagomapon@gmail.com>
2023-06-27 00:10:26 +0900

    section4-36 finish

--- 04_react_basic/src/120_props_rules/start/Example.js
+++ 04_react_basic/src/120_props_rules/start/Example.js
@@ -1,16 +1,34 @@
-// POINT propsの流れは一方通行
-// POINT propsは読み取り専用
-
 import Bye from "./components/Bye"
 import Hello from "./components/Hello"
 
 const Example = () => {
+  // POINT propsの流れは一方通行
+  const name = 'Tom';
+
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name}/>
+      <Bye name={name}/>
     </>
   );
 };
 
 export default Example;
+
+// 元々の記述
+// // POINT propsの流れは一方通行
+// // POINT propsは読み取り専用
+
+// import Bye from "./components/Bye"
+// import Hello from "./components/Hello"
+
+// const Example = () => {
+//   return (
+//     <>
+//       <Hello />
+//       <Bye />
+//     </>
+//   );
+// };
+
+// export default Example;
--- 04_react_basic/src/120_props_rules/start/components/Hello.js
+++ 04_react_basic/src/120_props_rules/start/components/Hello.js
@@ -1,4 +1,9 @@
 const Hello = (props) => {
+  // POINT propsは読み取り専用
+  // props.name = 'Bob';
+  const desc = Reflect.getOwnPropertyDescriptor(props, 'name');
+  console.log(desc);
+  // console.log(props.name);
   return (
     <div>
       <h3>Hello {props.name}</h3>
@@ -7,3 +12,14 @@ const Hello = (props) => {
 };
 
 export default Hello;
+
+// 元々の記述
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       <h3>Hello {props.name}</h3>
+//     </div>
+//   );
+// };
+
+// export default Hello;
