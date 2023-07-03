7fc62c3ac67aae9f1dda98b862b6af12ed449ad9
coma-dwc <tamagomapon@gmail.com>
2023-07-03 12:31:25 +0900

    section5-41 finish

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,7 +1,24 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('ボタンがクリックされました。');
+  }
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button>クリックしてね</button>
+    </>
   );
 };
 
 export default Example;
+
+
+// 元々の記述
+
+// const Example = () => {
+//   return (
+//     <></>
+//   );
+// };
+
+// export default Example;
