78f01930d112a136b9113c64fd37dfefc9205666
coma-dwc <tamagomapon@gmail.com>
2023-07-03 13:02:42 +0900

    section5-43 finish

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,10 +1,31 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  let valArry = useState();   
   return (
     <>
-      
+      <input 
+      type="text"
+      onChange={(e) => {
+        const setFn = valArry[1];  //配列の1番目には更新用の値
+        setFn(e.target.value)
+      }} /> = {valArry[0]}    //配列の0番目には参照用の値
     </>
   );
 };
 
 export default Example;
+
+
+// 元々の記述
+
+// const Example = () => {
+
+//   return (
+//     <>
+      
+//     </>
+//   );
+// };
+
+// export default Example;
