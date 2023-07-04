987d5daea8ae5930f998d01882b53cb6d4288fb7
coma-dwc <tamagomapon@gmail.com>
2023-07-04 23:34:27 +0900

    section5-47 finish

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,37 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+    const [ count, setCount ] = useState(0);
+    const countUp = () => {
+      setCount(count + 1);  
+      setCount(prevstate => prevstate + 1);
+    };
+
+    const countDown = () => {
+      setCount(count - 1);  
+    };
+    return (
+        <>
+            <p>現在のカウント数： {count}</p>
+            <button
+              onClick={countUp}
+            >+</button>
+
+            <button
+              onClick={countDown}
+            >-</button>
+        </>
+    );
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Example = () => {
+  
+// };
+
+// export default Example;
