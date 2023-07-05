08474357486193743e43df9a8f46906812509f86
coma-dwc <tamagomapon@gmail.com>
2023-07-05 11:27:35 +0900

    section5-53 finish

--- 05_state_and_event/src/070_state_and_component/start/Example.js
+++ 05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,6 +1,18 @@
 import { useState } from "react";
 
 const Example = () => {
+  const [ toggle, setToggle ] = useState(true);
+  const toggleComponent = () => {
+    setToggle(prev => !prev)
+  }
+  return(
+    <>
+    <button onClick={toggleComponent}>toggle</button>
+    {toggle ? <Count key="A" title="A"/> : <Count key="B" title="B"/>}
+    </>
+  )
+}
+const Count = ({ title }) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
@@ -10,7 +22,7 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>{title}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
@@ -18,3 +30,26 @@ const Example = () => {
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Example = () => {
+//   const [count, setCount] = useState(0);
+//   const countUp = () => {
+//     setCount((prevstate) => prevstate + 1);
+//   };
+//   const countDown = () => {
+//     setCount(count - 1);
+//   };
+//   return (
+//     <>
+//       <h3>カウント: {count}</h3>
+//       <button onClick={countUp}>+</button>
+//       <button onClick={countDown}>-</button>
+//     </>
+//   );
+// };
+
+// export default Example;
