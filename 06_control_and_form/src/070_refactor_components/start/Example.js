7a51253b9951cadc061d2d565dda60ac084083f3
coma-dwc <tamagomapon@gmail.com>
2023-07-08 00:57:58 +0900

    section6-65 finish

--- 06_control_and_form/src/070_refactor_components/start/Example.js
+++ 06_control_and_form/src/070_refactor_components/start/Example.js
@@ -1,34 +1,61 @@
 import { useState } from "react";
 
+import AnimalList from "./components/AnimalList";
+import AnimalFilter from "./components/AnimalFilter";
+
 const Example = () => {
   const animals = ["Dog", "Cat", "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
+  const filterdAnimals = animals.filter((animal) => {
+    const isMatch = animal.indexOf(filterVal) !== -1;
+    return isMatch;
+  });
+
   return (
     <>
-      <input
-        type="text"
-        value={filterVal}
-        onChange={(e) => setFilterVal(e.target.value)}
-      />
-      <ul>
-        {animals
-          .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            return isMatch;
-          })
-          .map((animal) => {
-            return (
-              <li key={animal}>
-                {animal}
-                {animal === "Dog" && "★"}
-              </li>
-            );
-          })}
-      </ul>
+      <AnimalFilter filterState={[filterVal, setFilterVal]} />
+      <AnimalList animals={filterdAnimals} />
     </>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Example = () => {
+//   const animals = ["Dog", "Cat", "Rat"];
+
+//   const [filterVal, setFilterVal] = useState("");
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         value={filterVal}
+//         onChange={(e) => setFilterVal(e.target.value)}
+//       />
+//       <ul>
+//         {animals
+//           .filter((animal) => {
+//             const isMatch = animal.indexOf(filterVal) !== -1;
+//             return isMatch;
+//           })
+//           .map((animal) => {
+//             return (
+//               <li key={animal}>
+//                 {animal}
+//                 {animal === "Dog" && "★"}
+//               </li>
+//             );
+//           })}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default Example;
