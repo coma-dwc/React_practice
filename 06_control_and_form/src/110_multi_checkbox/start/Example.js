2bd03d2f4307dcdfb1d5c4ee4f7acbb96b48fce0
coma-dwc <tamagomapon@gmail.com>
2023-07-08 23:59:03 +0900

    section6-69 finish

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -9,12 +9,79 @@ const Example = () => {
 
   const [sum, setSum] = useState(0);
 
+  const handleChange = (e) => {
+    const newFruits = fruits.map(fruit => {
+      const newFruit = { ...fruit };
+      if(newFruit.label === e.target.value) {
+        newFruit.checked = !fruit.checked;
+      }
+
+      return newFruit;
+    })
+    
+    setFruits(newFruits);
+
+    // let sumVal = 0;
+    // newFruits.forEach(fruit => {
+    //   if(fruit.checked) {
+    //     sumVal += fruit.value;
+    //   }
+    // });
+
+    // newFruits
+    // .filter((fruit) => fruit.checked)
+    // .forEach((fruit) => (sumVal = sumVal + fruit.value));
+
+    let sumVal = newFruits
+    .filter((fruit) => fruit.checked)
+    .reduce((sumVal, fruit) => sumVal + fruit.value, 0);  //第二引数の0が初期値
+
+    setSum(sumVal);
+  };
+
   return (
     <div>
-      
+      {fruits.map(fruit => {
+        return (
+            <div ket={fruit.label}>
+              <input 
+              id={fruit.label}
+                type="checkbox" 
+                value={fruit.label}
+                checked={fruit.checked}
+                onChange={handleChange} />
+              <label htmlFor={fruit.label}>
+                {fruit.label}:{fruit.value}
+              </label>
+            </div>
+        );
+      })}
       <div>合計：{sum}</div>
     </div>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Example = () => {
+//   const [fruits, setFruits] = useState([
+//     { label: "Apple", value: 100, checked: false },
+//     { label: "Banana", value: 200, checked: false },
+//     { label: "Cherry", value: 300, checked: false },
+//   ]);
+
+//   const [sum, setSum] = useState(0);
+
+//   return (
+//     <div>
+      
+//       <div>合計：{sum}</div>
+//     </div>
+//   );
+// };
+
+// export default Example;
