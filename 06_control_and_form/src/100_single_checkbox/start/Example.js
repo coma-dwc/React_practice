6cb93bce0a04321f2f6f95b3f29c3f237722b8ec
coma-dwc <tamagomapon@gmail.com>
2023-07-08 23:05:46 +0900

    section6-68 finish

--- 06_control_and_form/src/100_single_checkbox/start/Example.js
+++ 06_control_and_form/src/100_single_checkbox/start/Example.js
@@ -1,11 +1,41 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [isChecked, setIsChecked] = useState(true);
+
+  const toggleChecked = (e) => {
+    setIsChecked(prevState => !prevState);
+  };
+  
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <label htmlFor="my-check">
+        チェック：
+        </label>
+        <input
+          type="checkbox"
+          id="my-check"
+          checked={isChecked}
+          onChange={() => setIsChecked(prevState => !
+          prevState)}
+        />
+        <div>{isChecked ? "ON!" : "OFF!"}</div>
+    </div>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// const Example = () => {
+//   return (
+//     <p style={{ textAlign: "center" }}>
+//       startフォルダの内容が表示されます。
+//       <br />
+//       練習用に使ってください！
+//     </p>
+//   );
+// };
+
+// export default Example;
