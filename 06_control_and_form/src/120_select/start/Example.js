e92c0134a4c749b0afeb1f33a1e170184c6888f8
coma-dwc <tamagomapon@gmail.com>
2023-07-09 21:56:38 +0900

    section6-70 finish

--- 06_control_and_form/src/120_select/start/Example.js
+++ 06_control_and_form/src/120_select/start/Example.js
@@ -1,11 +1,39 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [selected, setSelected] = useState("Banana");
+
+  const OPTIONS = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <select
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+      >
+        {OPTIONS.map(opt => <option key={opt} value={opt}>
+        {opt}</option>)}
+        {/* <option value="Apple">Apple</option>
+        <option value="Banana">Banana</option>
+        <option value="Cherry">Cherry</option> */}
+      </select>
+      <div>選択された果物：</div>
+    </>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// const Example = () => {
+//   return (
+//     <p style={{ textAlign: "center" }}>
+//       startフォルダの内容が表示されます。
+//       <br />
+//       練習用に使ってください！
+//     </p>
+//   );
+// };
+
+// export default Example;
