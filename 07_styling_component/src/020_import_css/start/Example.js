2c26de238535e9ae38661b9fdeb6337e0e38ca7f
coma-dwc <tamagomapon@gmail.com>
2023-07-10 22:34:12 +0900

    section7-75 finish

--- 07_styling_component/src/020_import_css/start/Example.js
+++ 07_styling_component/src/020_import_css/start/Example.js
@@ -1,27 +1,21 @@
 import { useState } from "react";
 
+import SubButton from "./components/SubButton";
+import "./Example.css";
+
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
-  const style = {
-    margin: "auto",
-    "border-radius": "9999px",
-    border: "none",
-    display: "block",
-    width: 120,
-    height: 60,
-    fontWeight: "bold",
-    cursor: "pointer",
-    backgroundColor: isSelected ? "pink" : "",
-  };
+
 
   return (
     <>
-      <button style={style} onClick={clickHandler}>
+      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
         ボタン
       </button>
+      <SubButton />
       <div style={{ textAlign: "center" }}>
         {isSelected && "クリックされました。"}
       </div>
@@ -30,3 +24,38 @@ const Example = () => {
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Example = () => {
+//   const [isSelected, setIsSelected] = useState(false);
+
+//   const clickHandler = () => setIsSelected((prev) => !prev);
+
+//   const style = {
+//     margin: "auto",
+//     "border-radius": "9999px",
+//     border: "none",
+//     display: "block",
+//     width: 120,
+//     height: 60,
+//     fontWeight: "bold",
+//     cursor: "pointer",
+//     backgroundColor: isSelected ? "pink" : "",
+//   };
+
+//   return (
+//     <>
+//       <button style={style} onClick={clickHandler}>
+//         ボタン
+//       </button>
+//       <div style={{ textAlign: "center" }}>
+//         {isSelected && "クリックされました。"}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Example;
