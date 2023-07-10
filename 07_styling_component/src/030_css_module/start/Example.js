2c911734534fcbafa7de3e442ee72e0371f9e4cd
coma-dwc <tamagomapon@gmail.com>
2023-07-10 22:52:02 +0900

    section7-76 finish

--- 07_styling_component/src/030_css_module/start/Example.js
+++ 07_styling_component/src/030_css_module/start/Example.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 import SubButton from "./components/SubButton";
-import "./Example.css";
+import styles from "./Example.module.css";
+
+// console.log(styles);
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -10,7 +12,7 @@ const Example = () => {
 
   return (
     <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+      <button className={`${styles.btn} ${isSelected ? styles.selected : ""}`} onClick={clickHandler}>
         ボタン
       </button>
       <SubButton />
@@ -22,3 +24,30 @@ const Example = () => {
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// import SubButton from "./components/SubButton";
+// import "./Example.css";
+
+// const Example = () => {
+//   const [isSelected, setIsSelected] = useState(false);
+
+//   const clickHandler = () => setIsSelected((prev) => !prev);
+
+//   return (
+//     <>
+//       <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+//         ボタン
+//       </button>
+//       <SubButton />
+//       <div style={{ textAlign: "center" }}>
+//         {isSelected && "クリックされました。"}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Example;
