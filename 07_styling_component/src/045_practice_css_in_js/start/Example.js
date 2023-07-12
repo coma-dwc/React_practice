87965d30fee65c99bff5b4a0070fff05d65ef9c7
coma-dwc <tamagomapon@gmail.com>
2023-07-12 22:18:52 +0900

    section7-79 finish

--- 07_styling_component/src/045_practice_css_in_js/start/Example.js
+++ 07_styling_component/src/045_practice_css_in_js/start/Example.js
@@ -7,7 +7,17 @@ const FirstButton = styled.button`
   margin: 0.5rem 1rem;
   width: 11rem;
   border: none;
+  background: pink;
 `;
+
+const SecondButton = styled(FirstButton)`
+  background: red;
+  color: white;
+`;
+
+const ThirdButton = styled(SecondButton)`
+  background: ${({ dark }) => dark ? "black" : "green"};
+`
 
 const Example = () => {
   return (
@@ -28,8 +38,50 @@ const Example = () => {
         </ul>
       </p>
       <FirstButton>ボタン1</FirstButton>
+      <SecondButton>ボタン2</SecondButton>
+      <ThirdButton>ボタン3</ThirdButton>
+      <ThirdButton dark>ボタン3</ThirdButton>
     </>
   );
 };
 
 export default Example;
+
+
+// 元々の記述
+
+// import styled from "styled-components";
+
+// const FirstButton = styled.button`
+//   display: inline-block;
+//   border-radius: 3px;
+//   padding: 0.5rem 0;
+//   margin: 0.5rem 1rem;
+//   width: 11rem;
+//   border: none;
+// `;
+
+// const Example = () => {
+//   return (
+//     <>
+//       <h3>練習問題</h3>
+//       <p>
+//         記述を変更し、完成コードと同じ状態になるようにしてください。
+//         <ul>
+//           <li>Q1. FirstButtonのbackgroudをpinkにしてください。</li>
+//           <li>
+//             Q2.
+//             FirstButtonを継承したSecondButtonを作成し、backgroudをredに、colorをwhiteにしてください。{" "}
+//           </li>
+//           <li>
+//             Q3.
+//             SecondButtonを継承したThirdButtonを作成し、props.darkがある場合のみbackgroudがblackに、ない場合はgreenになるようにしてください。
+//           </li>
+//         </ul>
+//       </p>
+//       <FirstButton>ボタン1</FirstButton>
+//     </>
+//   );
+// };
+
+// export default Example;
