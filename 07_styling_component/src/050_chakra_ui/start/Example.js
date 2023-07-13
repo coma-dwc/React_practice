697da732a2afdea588f8a249fd348f0f1867788b
coma-dwc <tamagomapon@gmail.com>
2023-07-13 20:15:31 +0900

    section7-82 finish

--- 07_styling_component/src/050_chakra_ui/start/Example.js
+++ 07_styling_component/src/050_chakra_ui/start/Example.js
@@ -1,12 +1,31 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
 import Todo from "./components/Todo"
 
 const Example = () => {
   return (
     <>
-      <h2>Reminder</h2>
+    <ChakraProvider>
       <Todo />
+    </ChakraProvider>
     </>
   );
 };
 
 export default Example;
+
+
+// 元々の記述
+
+// import Todo from "./components/Todo"
+
+// const Example = () => {
+//   return (
+//     <>
+//       <h2>Reminder</h2>
+//       <Todo />
+//     </>
+//   );
+// };
+
+// export default Example;
--- 07_styling_component/src/050_chakra_ui/start/components/List.js
+++ 07_styling_component/src/050_chakra_ui/start/components/List.js
@@ -1,9 +1,19 @@
+import { VStack, StackDivider } from "@chakra-ui/react";
+
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
-        <div>
+        <VStack
+                divider={<StackDivider />}
+                // color={{ sm: 'red.600', md: 'blue.600', lg: 'green.500', xl: 'red.600'}}
+                borderColor="black"
+                borderWidth="1px"
+                borderRadius="3px" //border-radius: 3px
+                p={5}  //p = padding
+                alignItems="start"
+            >
             {todos.map(todo => {
                 return (
                     <div key={todo.id}>
@@ -12,8 +22,30 @@ const List = ({todos, deleteTodo}) => {
                     </div>
                 )
             })}
-        </div>
+        </VStack>
     );
 }
 
 export default List;
+
+// 元々の記述
+
+// const List = ({todos, deleteTodo}) => {
+//     const complete = (id) => {
+//         deleteTodo(id)
+//     }
+//     return (
+//         <div>
+//             {todos.map(todo => {
+//                 return (
+//                     <div key={todo.id}>
+//                         <button onClick={() => complete(todo.id)}>完了</button>
+//                         <span>{todo.content}</span>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//     );
+// }
+
+// export default List;
