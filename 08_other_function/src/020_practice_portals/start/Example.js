50bf1a1988ade5451fe513331310d98b6594c5ca
coma-dwc <tamagomapon@gmail.com>
2023-07-16 19:21:22 +0900

    section8-87 finish

--- 08_other_function/src/020_practice_portals/start/Example.js
+++ 08_other_function/src/020_practice_portals/start/Example.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
+
 import Toast from "./components/Toast";
+const ToastPortal = ({ children }) => {
+  const target = document.querySelector('.container.start')
+  return createPortal(children, target);
+}
 
 const Example = () => {
   const [toastOpen, setToastOpen] = useState(false);
@@ -21,13 +27,51 @@ const Example = () => {
         トーストを表示する
       </button>
       {toastOpen && (
+        <ToastPortal>
           <Toast
             visible={toastOpen}
             handleCloseClick={() => setToastOpen(false)}
           />
+        </ToastPortal>
       )}
     </div>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+// import Toast from "./components/Toast";
+
+// const Example = () => {
+//   const [toastOpen, setToastOpen] = useState(false);
+
+//   return (
+//     <div>
+//       <h3>
+//         トーストの作成（createPortal）
+//       </h3>
+//       <p>ボタンを押すと.container.start要素にマウントされて、表示されるトーストを作成してください。トーストにはToastコンポーネントを使用してください。</p>
+
+//       <div className="container start"></div>
+      
+//       <button
+//         type="button"
+//         onClick={() => setToastOpen(true)}
+//         disabled={toastOpen}
+//       >
+//         トーストを表示する
+//       </button>
+//       {toastOpen && (
+//           <Toast
+//             visible={toastOpen}
+//             handleCloseClick={() => setToastOpen(false)}
+//           />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Example;
--- 08_other_function/src/020_practice_portals/start/components/Toast.js
+++ 08_other_function/src/020_practice_portals/start/components/Toast.js
@@ -19,3 +19,27 @@ const Toast = ({ visible, handleCloseClick }) => {
 };
 
 export default Toast;
+
+// 元々の記述
+
+// import "./Toast.css";
+
+// const Toast = ({ visible, handleCloseClick }) => {
+//   const toastClassName = visible ? "toast is-visible" : "toast";
+//   return (
+//     <div className={toastClassName}>
+//       <div className="toast__content">
+//         <p>トースト</p>
+//         <button
+//           type="button"
+//           className="toast__button"
+//           onClick={handleCloseClick}
+//         >
+//           ×
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Toast;
