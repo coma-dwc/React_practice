5ee3ae511d3f40125ae9a692c9e7eeec5eca4b72
coma-dwc <tamagomapon@gmail.com>
2023-07-16 19:31:17 +0900

    section8-88 finish

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef()
+
+  // console.log(inputRef);
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button>
     </div>
@@ -23,3 +26,31 @@ const Example = () => {
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Case1 = () => {
+//   const [value, setValue] = useState("");
+
+//   return (
+//     <div>
+//       <h3>ユースケース1</h3>
+//       <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+//       <button onClick>
+//         インプット要素をフォーカスする
+//       </button>
+//     </div>
+//   );
+// };
+
+// const Example = () => {
+//   return (
+//     <>
+//       <Case1 />
+//     </>
+//   );
+// };
+
+// export default Example;
