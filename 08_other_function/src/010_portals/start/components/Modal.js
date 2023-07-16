3aabf47de6874b4373cad9c1671183c2de8f3a5d
coma-dwc <tamagomapon@gmail.com>
2023-07-16 18:59:09 +0900

    section8-86 finish

--- 08_other_function/src/010_portals/start/components/Modal.js
+++ 08_other_function/src/010_portals/start/components/Modal.js
@@ -14,3 +14,22 @@ const Modal = ({ handleCloseClick }) => {
 };
 
 export default Modal;
+
+// 元々の記述
+
+// import "./Modal.css";
+
+// const Modal = ({ handleCloseClick }) => {
+//   return (
+//     <div className="modal">
+//       <div className="modal__content">
+//         <p>モーダル</p>
+//         <button type="button" onClick={handleCloseClick}>
+//           閉じる
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Modal;
