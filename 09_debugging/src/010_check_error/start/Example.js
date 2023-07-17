46b5c9ac9bc4ca930c0434822780a08f87a92733
coma-dwc <tamagomapon@gmail.com>
2023-07-17 22:41:59 +0900

    section9-96 finish

--- 09_debugging/src/010_check_error/start/Example.js
+++ 09_debugging/src/010_check_error/start/Example.js
@@ -1,17 +1,49 @@
+import { useState } from "react"
+
 const Example = () => {
-  // const [count, setCount] = useState({ val: 0 });
+  const [count, setCount] = useState({ val: 0 });
 
-  // const countUp = () => {
-  //   setCount((prevstate) => {
-  //     prevstate.val += 1;
-  //     return prevstate;
-  //   });
-  // };
+  console.log('re-render') //再レンダリングされていないから画面が更新されない
 
-  // return (
-  //     <p>現在のカウント数: {count}</p>
-  //     <button onClick={countUp}>+</button>
-  // );
+  const countUp = () => {
+    // console.log('button clicked')
+    setCount((prevstate) => {
+      console.log('button clicked');
+      const newState = { val: prevstate.val + 1 };
+
+      // newState.val += 1;
+      // console.log(prevstate)
+      return newState;
+    });
+  };
+
+  return (
+    <>
+      <p>現在のカウント数: {count.val}</p>
+      <button onClick={countUp}>+</button>
+    </>
+  );
 };
 
 export default Example;
+
+
+// 元々の記述
+
+// const Example = () => {
+//   // const [count, setCount] = useState({ val: 0 });
+
+//   // const countUp = () => {
+//   //   setCount((prevstate) => {
+//   //     prevstate.val += 1;
+//   //     return prevstate;
+//   //   });
+//   // };
+
+//   // return (
+//   //     <p>現在のカウント数: {count}</p>
+//   //     <button onClick={countUp}>+</button>
+//   // );
+// };
+
+// export default Example;
