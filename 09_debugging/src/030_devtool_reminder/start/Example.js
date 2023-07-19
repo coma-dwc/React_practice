fc8ad14fce9f38aa5e382f1f36e276804031553b
coma-dwc <tamagomapon@gmail.com>
2023-07-19 23:32:23 +0900

    section9-98 finish

--- 09_debugging/src/030_devtool_reminder/start/Example.js
+++ 09_debugging/src/030_devtool_reminder/start/Example.js
@@ -7,3 +7,15 @@ const Example = () => {
 };
 
 export default Example;
+
+// 元々の記述
+
+// const Example = () => {
+//   return (
+//     <>
+//       <h3>React Developer Toolsを使ってみよう</h3>
+//     </>
+//   );
+// };
+
+// export default Example;
