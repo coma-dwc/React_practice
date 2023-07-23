b4eb430ed29f271ca16cac0953e2cf3ae55d5980
coma-dwc <tamagomapon@gmail.com>
2023-07-23 22:26:29 +0900

    section10-107 finish

--- 10_functional_programming/src/050_react_data_procedure/start/Example.js
+++ 10_functional_programming/src/050_react_data_procedure/start/Example.js
@@ -1,10 +1,21 @@
 import { useState } from "react";
 
+// 状態管理と処理を分離
+
+// 関数型（純粋関数）
+// ・fn(決まった引数) -> 決まった戻り値
+// ・関数外の状態（データ）は参照・変更しない
+// ・関数外に影響を及ぼさない
+// ・引数で渡された値を変更しない
+
+let value = 0;
 const Example = () => {
+  // JSXを作成する場所
   const [ state, setState ] = useState(0);
   const increment = () => {
     setState(state + 1);
   }
+  window.alert('hello')  //副作用になるようなものはここ(JSXを作成する場所)には書かない
   return (
     <>
       <button onClick={increment}>+</button>
@@ -14,3 +25,22 @@ const Example = () => {
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Example = () => {
+//   const [ state, setState ] = useState(0);
+//   const increment = () => {
+//     setState(state + 1);
+//   }
+//   return (
+//     <>
+//       <button onClick={increment}>+</button>
+//       <h3>{state}</h3>
+//     </>
+//   );
+// };
+
+// export default Example;
