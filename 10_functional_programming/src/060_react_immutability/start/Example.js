5bb441d11c7813bcc95fc4a93f53f6b0050add5f
coma-dwc <tamagomapon@gmail.com>
2023-07-23 22:43:11 +0900

    section10-108 finish

--- 10_functional_programming/src/060_react_immutability/start/Example.js
+++ 10_functional_programming/src/060_react_immutability/start/Example.js
@@ -1,9 +1,25 @@
 import { useState } from "react";
 
-const Child = (props) => {
+// 関数型（純粋関数）
+// ・fn（決まった引数）-> 決まった戻り値
+// ・関数外の状態（データ）は参照・変更しない
+// ・関数外に影響を及ぼさない
+// ・引数で渡された値を変更しない (propsを変更しない)　（★Immutability）
+
+const Child = ({ state, setState }) => {
+  // setState({ value: 1 })
+  // props.state = { value: 1 }
+  const increment = () => {
+    setState(prev => {
+      const newState = { value: prev.value + 1 }
+      // prev.value += 1;
+      return newState;
+    })
+  }
   return (
     <>
-      <span>{props.state.value}</span>
+      <span>{state.value}</span>
+      <button onClick={increment}>+</button>
     </>
   );
 };
@@ -14,10 +30,36 @@ const Example = () => {
   return (
     <>
       <div>
-        <Child state={state}/>
+        <Child state={state} setState={setState}/>
       </div>
     </>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// import { useState } from "react";
+
+// const Child = (props) => {
+//   return (
+//     <>
+//       <span>{props.state.value}</span>
+//     </>
+//   );
+// };
+
+// const Example = () => {
+//   const [ state, setState ] = useState({ value: 0 });
+
+//   return (
+//     <>
+//       <div>
+//         <Child state={state}/>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Example;
