8c5b0628bf5e06b9730131ec5564e3c5523d9788
coma-dwc <tamagomapon@gmail.com>
2023-08-09 14:21:32 +0900

    section11-123 finish

--- 11_hooks_p1/src/080_practice_reminder/start/components/List.js
+++ 11_hooks_p1/src/080_practice_reminder/start/components/List.js
@@ -1,19 +1,37 @@
-const List = ({todos, deleteTodo}) => {
+import Item from "./Item";
+
+const List = ({todos, deleteTodo, updateTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
         <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
+            {todos.map(todo => <Item todo={todo} complete={complete} 
+            key={todo.id} updateTodo={updateTodo}/>)}
         </div>
     );
 }
 
 export default List;
+
+// 元々の記述
+
+// const List = ({todos, deleteTodo}) => {
+//     const complete = (id) => {
+//         deleteTodo(id)
+//     }
+//     return (
+//         <div>
+//             {todos.map(todo => {
+//                 return (
+//                     <div key={todo.id}>
+//                         <button onClick={() => complete(todo.id)}>完了</button>
+//                         <span>{todo.content}</span>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//     );
+// }
+
+// export default List;
--- 11_hooks_p1/src/080_practice_reminder/start/components/Todo.js
+++ 11_hooks_p1/src/080_practice_reminder/start/components/Todo.js
@@ -7,14 +7,17 @@ const Todo = () => {
     {
       id: 1,
       content: "店予約する",
+      editing: false,
     },
     {
       id: 2,
       content: "卵買う",
+      editing: false,
     },
     {
       id: 3,
       content: "郵便出す",
+      editing: false,
     },
   ];
 
@@ -32,11 +35,63 @@ const Todo = () => {
     setTodos([...todos, todo]);
   }
 
+  const updateTodo = (todo) => {
+    const newTodos = todos.map(_todo => {
+      return _todo.id === todo.id ? { ..._todo, ...todo } : { ..._todo };
+    });
+    setTodos(newTodos);
+  }
+
   return (
     <>
-      <List todos={todos} deleteTodo={deleteTodo}/>
+      <List todos={todos} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
       <Form createTodo={createTodo}/>
     </>
   )
 };
 export default Todo;
+
+// 元々の記述
+
+// import { useState } from "react"
+// import List from "./List"
+// import Form from "./Form"
+
+// const Todo = () => {
+//   const todosList = [
+//     {
+//       id: 1,
+//       content: "店予約する",
+//     },
+//     {
+//       id: 2,
+//       content: "卵買う",
+//     },
+//     {
+//       id: 3,
+//       content: "郵便出す",
+//     },
+//   ];
+
+//   const [ todos, setTodos ] = useState(todosList);
+
+//   const deleteTodo = (id) => {
+//     const newTodos = todos.filter((todo) => {
+//       return todo.id !== id;
+//     });
+
+//     setTodos(newTodos);
+//   }
+
+//   const createTodo = (todo) => {
+//     setTodos([...todos, todo]);
+//   }
+
+//   return (
+//     <>
+//       <List todos={todos} deleteTodo={deleteTodo}/>
+//       <Form createTodo={createTodo}/>
+//     </>
+//   )
+// };
+// export default Todo;
