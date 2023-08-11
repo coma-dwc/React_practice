2df78e3a78cfaaed0a7e67dda17624fcb103b679
coma-dwc <tamagomapon@gmail.com>
2023-08-11 23:37:17 +0900

    section12-134 finish

--- 12_hooks_p2/src/075_practice_customHook/start/Example.js
+++ 12_hooks_p2/src/075_practice_customHook/start/Example.js
@@ -1,4 +1,8 @@
+import { useCount } from './hooks';
+
 const Example = () => {
+  const { count, countUp } = useCount();
+
   return (
     <>
       <h3>練習問題</h3>
@@ -6,10 +10,28 @@ const Example = () => {
         記述を変更し、完成コードと同じ状態になるようにしてください。
         startフォルダの中にhooks.jsというファイルを作成しその中でuseCountというカスタムフックを作成してください。
       </p>
-      <div>Counts: {}</div>
-      <button onClick={() => {}}>Count Up!</button>
+      <div>Counts: {count}</div>
+      <button onClick={countUp}>Count Up!</button>
     </>
   );
 };
 
 export default Example;
+
+// 元々の記述
+
+// const Example = () => {
+//   return (
+//     <>
+//       <h3>練習問題</h3>
+//       <p>
+//         記述を変更し、完成コードと同じ状態になるようにしてください。
+//         startフォルダの中にhooks.jsというファイルを作成しその中でuseCountというカスタムフックを作成してください。
+//       </p>
+//       <div>Counts: {}</div>
+//       <button onClick={() => {}}>Count Up!</button>
+//     </>
+//   );
+// };
+
+// export default Example;
